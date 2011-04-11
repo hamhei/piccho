4a38ae362a8259ec11f2b3c2a3f1c4beb133e38a
hamhei <hamhei@hamhamworld.net>
2011-04-11 03:25:58 -0700

    Authorize function. bug fix. ver.1.0

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import logging
 import os
 import math
+import appengine_utilities.sessions
 
 from google.appengine.dist import use_library
 from google.appengine.ext import db
@@ -15,12 +16,14 @@ from google.appengine.api import images
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 from getimageinfo import getImageInfo
+from appengine_utilities.sessions import Session
 
 #use_library('django', '1.2')
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 logging.getLogger().setLevel(logging.DEBUG)
 #tag = "no tag"
 #name = "no name"
+session = Session()
 
 class Greeting(db.Model):
 	author = db.StringProperty(multiline=False)
@@ -29,6 +32,7 @@ class Greeting(db.Model):
 	avatar = db.BlobProperty()
 	tag = db.StringProperty(multiline=False)
 	date = db.DateTimeProperty(auto_now_add=True)
+	#modify = db.DateTimeProperty()
 
 class Tweet(db.Model):
 	content = db.StringProperty(multiline=False)
@@ -36,45 +40,71 @@ class Tweet(db.Model):
 	date = db.DateTimeProperty(auto_now_add=True)
 	img = db.StringProperty(multiline=False)
 
+class Room(db.Model):
+	isPass = db.BooleanProperty(default=True)
+	password = db.StringProperty(multiline=False)
+
 class LoginPage(webapp.RequestHandler):
 	def get(self):
-  # global tag, name
-	#	tag = "no tag"
-	#	name = "no name"
+		#session.delete()
+		session["name"] = "no name"
+		session["pass"] = "no pass"
 		path = os.path.join(os.path.dirname(__file__), 'index.html')
 		template_value = {}
 		self.response.out.write(template.render(path, template_value))
 
+class Phrase(webapp.RequestHandler):
+	def post(self):
+		if len(self.request.get("tag")) > 0:
+			session["tag"] = self.request.get("tag")
+			session["name"] = self.request.get("name")
+		room = Room.get_or_insert(session["tag"])
+		room.put()
+		self.redirect('/main')
+
+class Check(webapp.RequestHandler):
+	def post(self):
+		session["pass"] = self.request.get("pass")
+		self.redirect('/main')
+
 class MainPage(webapp.RequestHandler):
-	def get(self, tag, name):
-		#global tag,name
-		#greetings = db.GqlQery("SELECT * FROM Greeting WHERE tag = :1 ORDER BY date ASC", tag)
-		#tag = self.get.request('tag')
-		#name = self.get.request('name')
+	def get(self):
+		if 'tag' in session:
+			pass
+		else:
+			self.response.out.write("<font color='ff0000'> Enter Any Tag. </font>")
+			self.redirect("/")
 
-		greetings = Greeting.gql("WHERE tag = :1 ORDER BY date ASC", tag)
+		room = Room.get_by_key_name(session["tag"])
+		if room.isPass == False  and  session["pass"] != room.password:
+				self.response.out.write("""Password: <form action='/check' method='post'><textarea name='pass' rows='1' cols='10'></textarea><input type='submit' value='submit'></form>""")
 
-		tweets_list = []
-		for greeting in greetings:
-			temps = Tweet.gql("WHERE img = :1 ORDER BY date ASC", str(greeting.key()))
-			tweets_list.append(temps)
+		else:
+			isPass = room.isPass
+			greetings = Greeting.gql("WHERE tag = :1 ORDER BY date DESC", session["tag"])
 
-		lists = zip(greetings, tweets_list)
+			tweets_list = []
+			for greeting in greetings:
+				temps = Tweet.gql("WHERE img = :1 ORDER BY date ASC", str(greeting.key()))
+				tweets_list.append(temps)
 
-		names = set([])
-		for greeting in greetings:
-			names |= set([greeting.author])
-		menbers = list(names)
-		menbers.sort()
+			lists = zip(greetings, tweets_list)
 
-		template_value = {
-			'tag': tag,
-			'name': name,
-			'menbers': menbers,
-			'lists': lists,
-			}
-		path = os.path.join(os.path.dirname(__file__), 'main.html')
-		self.response.out.write(template.render(path, template_value))
+			names = set([])
+			for greeting in greetings:
+				names |= set([greeting.author])
+			members = list(names)
+			members.sort()
+
+			template_value = {
+				'tag': session["tag"],
+				'name': session["name"],
+				'members': members,
+				'lists': lists,
+				'isPass': isPass,
+				}
+			path = os.path.join(os.path.dirname(__file__), 'main.html')
+			self.response.out.write(template.render(path, template_value))
 
 class Image (webapp.RequestHandler):
 	def get(self):
@@ -87,13 +117,10 @@ class Image (webapp.RequestHandler):
 
 class Guestbook(webapp.RequestHandler):
 	def post(self):
-		#global tag,name
 		greeting = Greeting()
 		#bug.... sometime missing tag and name.  why?
-		tag = self.request.get("tag")
-		name = self.request.get("name")
-		greeting.tag = tag
-		greeting.author = name
+		greeting.tag = session["tag"]
+		greeting.author = session["name"]
 		bin=db.Blob(self.request.get("img"))
 		content_type, width, height = getImageInfo(bin)
 		if width > 600:
@@ -106,47 +133,32 @@ class Guestbook(webapp.RequestHandler):
 		tweet = Tweet()
 		tweet.img = str(greeting.key())
 		tweet.content = self.request.get("content")
-		tweet.author = name
+		tweet.author = session["name"]
 		tweet.put()
 
-		self.redirect('/main/'+tag+'/'+name)
-
-class Phrase(webapp.RequestHandler):
-	def post(self):
-		#global tag,name
-		tag = self.request.get("tag", default_value="no tag")
-		name = self.request.get("name", default_value="no name")
-		self.redirect('/main/'+tag+'/'+name)
+		self.redirect('/main')
 
 class Change(webapp.RequestHandler):
 	def post(self):
-		#global tag
-		tag = self.request.get("tag")
-		name = self.request.get("name")
 		greetings = Greeting.gql("WHERE tag = :1", tag)
 		for greeting in greetings:
 			greeting.tag = self.request.get("newtag")
 			greeting.put()
-		tag = self.request.get("newtag")
-		self.redirect('/main/'+tag+'/'+name)
+		session["tag"] = self.request.get("newtag")
+		self.redirect('/main')
 
 class Comment(webapp.RequestHandler):
 	def post(self):
-		#global tag, name
-		name = self.request.get("name")
-		tag = self.request.get("tag")
 		tweet = Tweet()
 		tweet.img = str(self.request.get("key"))
 		tweet.content = self.request.get("comment")
-		tweet.author = name
+		tweet.author = session["name"]
 		tweet.put()
-		self.redirect('/main/'+tag+'/'+name)
+		self.redirect('/main')
 
 class Forgot(webapp.RequestHandler):
 	def post(self):
-		tag = self.request.get("tag")
-		name = self.request.get("name")
-		greetings = Greeting.gql("WHERE author = :1 ORDER BY date ASC", self.request.get("name"))
+		greetings = Greeting.gql("WHERE author = :1", self.request.get("name"))
 		tags = set([])
 		for greeting in greetings:
 			tags |= set([greeting.tag])
@@ -158,32 +170,46 @@ class Forgot(webapp.RequestHandler):
 		lists = zip(chars, length)
 		template_value = {
 			'name': self.request.get("name"),
-			'tags': tags,
 			'lists': lists,
 			}
 		path = os.path.join(os.path.dirname(__file__), 'forgot.html')
 		self.response.out.write(template.render(path, template_value))
 
+class Pass(webapp.RequestHandler):
+	def post(self):
+		room = Room.get_or_insert(session["tag"])
+		room.isPass = not room.isPass
+		if not room.isPass:
+			room.password = self.request.get("pass")
+			session["pass"] = room.password
+		room.put()
+		self.redirect('/main')
+
+class Order(webapp.RequestHandler):
+	def post(self):
+		self.redirect('/main')
+
 class Rmimg(webapp.RequestHandler):
 	def post(self):
-		tag = self.request.get("tag")
-		name = self.request.get("name")
 		greeting = db.get(self.request.get("key"))
 		tweets = Tweet.gql("WHERE img = :1 ", str(self.request.get("key")))
 		db.delete(greeting)
 		db.delete(tweets)
-		self.redirect('/main/'+tag+'/'+name)
+		self.redirect('/main')
 
 application = webapp.WSGIApplication([
 		('/', LoginPage),
-		('/main/([^/]*)/?(.*)', MainPage),
+		('/main', MainPage),
 		('/img', Image),
 		('/sign', Guestbook),
 		('/phrase', Phrase),
 		('/change', Change),
 		('/comment', Comment),
 		('/forgot', Forgot),
-		('/rmimg', Rmimg)
+		('/pass', Pass),
+		('/order', Order),
+		('/rmimg', Rmimg),
+		('/check', Check)
 		], debug=True)
 
 def main():
